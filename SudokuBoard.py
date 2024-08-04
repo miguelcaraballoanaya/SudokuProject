ce7995545e4d615427bc875b3e7ad482d9439874
Silverbullet69 <jeremiah.chitwood@gmail.com>
2024-08-04 13:52:59 -0600

    added board class

--- SudokuBoard.py
+++ SudokuBoard.py
@@ -1,1 +1,40 @@
+class Board:
 
+    def __init__(self, value, row, col, screen):
+        self.value = value
+        self.row = row
+        self.col = col
+        self.screen = screen
+
+    def draw(self):
+        pass
+
+    def select(self, row, col):
+        pass
+
+    def click(self, row, col):
+        pass
+
+    def clear(self):
+        pass
+
+    def sketch(self, value):
+        pass
+
+    def place_number(self, value):
+        pass
+
+    def reset_to_original(self):
+        pass
+
+    def is_full(self):
+        pass
+
+    def update_board(self):
+        pass
+
+    def find_empty(self):
+        pass
+
+    def check_board(self):
+        pass
